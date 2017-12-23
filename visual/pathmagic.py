ab26ba126673fb59d9a9beddf7f4eb845cc25d41
InDevRus <indevrus@hotmail.com>
2017-12-23 07:05:08 +0500

    Added pathmagic

---
+++ visual/pathmagic.py
@@ -0,0 +1,4 @@
+import sys
+from os import path
+
+sys.path.append(path.join(path.dirname(path.abspath(__file__)), path.pardir))
--- visual/visual.py
+++ visual/visual.py
@@ -1,3 +1,5 @@
+# noinspection PyUnresolvedReferences
+import pathmagic
 from sys import argv, exit
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
